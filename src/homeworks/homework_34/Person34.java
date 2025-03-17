a6346c7e176befc15cc5b71c62f14d52afa6e4a0
Panfilovs <maksims_panfilovs@yahoo.com>
2025-03-17 12:07:52 +0100

    les35

---
+++ src/homeworks/homework_34/Person34.java
@@ -0,0 +1,134 @@
+package homeworks.homework_34;
+
+public class Person34 {
+
+    private String email;
+    private String password;
+
+    public Person34(String email, String password) {
+        setEmail(email);
+        setPassword(password);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (isEmailValid(email)) {
+            this.email = email;
+        }
+    }
+
+    /*
+    1. Должна присутствовать @ и только одна
+    2. Точка после собаки
+    3. После последней точки есть 2 или более символов
+    4. Алфавит, цифры, '-', '_', '@', '.'
+    5. До собаки должен быть хотя бы один символ
+    6. Первый символ - должна быть буква
+     */
+    private boolean isEmailValid(String email) {
+
+        if (email == null) return false;
+
+        // 1. Должна присутствовать @
+        int indexAt = email.indexOf('@');
+        int lastAt = email.lastIndexOf('@');
+        if (indexAt == -1 || indexAt != lastAt) return false;
+
+        // 2. Точка после собаки
+        int dotIndexAfterAt = email.indexOf('.', indexAt + 1);
+        if (dotIndexAfterAt == -1) return false;
+
+        // 3. После последней точки есть 2 или более символов
+        int lastDotIndex = email.lastIndexOf('.');
+        if (lastDotIndex >= email.length() - 2) return false;
+
+        // 4. Алфавит, цифры, '-', '_', '@', '.'
+        /*
+        Перебираю все символы в строке. Проверяю текущий символ.
+        Если нахожу хоть один "не правильный" сразу возвращаем false
+         */
+        // string.toCharArray() -> char[]
+        for (char ch : email.toCharArray()) {
+            // проверка символа на правильность
+            boolean isPass = Character.isAlphabetic(ch)
+                    || Character.isDigit(ch)
+                    || ch == '-'
+                    || ch == '_'
+                    || ch == '.'
+                    || ch == '@';
+
+            // Если символ не подходит (isPass = false) возвращаем false
+            if (!isPass) return false; // делай что-то если переменная false
+        }
+
+        // 5. До собаки должен быть хотя бы один символ
+        if (indexAt == 0) return false;
+
+        // 6. Первый символ - должна быть буква
+        if (!Character.isLetter(email.charAt(0))) return false;
+
+        // Все проверки пройдены. Email подходит
+        return true;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (isPassValid(password)) {
+            this.password = password;
+        }
+    }
+
+    /*
+    Требования к паролю:
+    1. Длина пароля >= 8
+    2. Должна быть мин 1 цифра
+    3. Должна быть мин 1 маленькая буква -> Character.isLowerCase()
+    4. Должна быть мин 1 большая буква
+    5. Должна быть мин 1 специальный символ: "!%$@&*()[],.-"
+
+    Пароль должен удовлетворять ВСЕМ требованием сразу.
+    5 boolean переменных. Каждая отвечает за свой пункт.
+    Пароль будет подходить только если все пять - имеют true
+     */
+
+    public boolean isPassValid(String password) {
+        if (password == null || password.length() < 8) return false;
+
+        boolean isDigit = false;
+        boolean isUpperCase = false;
+        boolean isLowerCase = false;
+        boolean isSpecialSymbol = false;
+
+        // альтернативный способ объявление переменной
+        boolean[] result = new boolean[4]; // false, false, false, false
+
+        String symbols = "!%$@&*()[],.-";
+
+        // Перебираем символы
+        for (char ch : password.toCharArray()) {
+            if (Character.isDigit(ch)) isDigit = true;
+            if (Character.isLowerCase(ch)) isLowerCase = true;
+            if (Character.isUpperCase(ch)) isUpperCase = true;
+            if (symbols.indexOf(ch) >= 0) isSpecialSymbol = true;
+//            if (symbols.contains(String.valueOf(ch))) isSpecialSymbol = true;
+        }
+        System.out.printf("%s | %s | %s | %s\n", isDigit, isLowerCase, isUpperCase, isSpecialSymbol);
+
+        // Если хотя бы одна из переменных останется в значении false, то весь пароль НЕ будет признан валидным (вернется false).
+        return isDigit && isLowerCase && isUpperCase && isSpecialSymbol;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/homeworks/homework_34/Person34Test.java
@@ -0,0 +1,72 @@
+package homeworks.homework_34;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+public class Person34Test {
+
+    Person34 person;
+    String startEmail = "john@test.com";
+    String startPassword = "qwerty1Q%s";
+
+    @BeforeEach
+    void setUp() {
+        person = new Person34(startEmail, startPassword);
+    }
+
+    /*
+    1. Беру валидный email
+    2. Устанавливаем его сеттером person-у
+    3. Мы ожидаем: что у person в поле email будет значение, которое мы установили в шаге 2.
+     */
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"valid@test.com", "test111@test.com.de"})
+    void testValidEmailSet(String validEmail) {
+
+        person.setEmail(validEmail);
+
+        Assertions.assertEquals(validEmail, person.getEmail());
+    }
+
+    /*
+    1. Берем НЕ валидный email
+    2. Устанавливаем его сеттером.
+    3. Ожидаемый результат: Email установлен не будет
+        3.1. поле email у person НЕ будет равно "не валидный email"
+        3.2. Значение поля email должно остаться прежним
+     */
+
+    @ParameterizedTest
+    @MethodSource("invalidEmailData")
+    void testInvalidEmailSet(String invalidEmail) {
+
+        person.setEmail(invalidEmail);
+
+        Assertions.assertNotEquals(invalidEmail, person.getEmail());
+        Assertions.assertEquals(startEmail, person.getEmail());
+
+    }
+
+    static Stream<String> invalidEmailData() {
+        return Stream.of(
+                "testmail.net",
+                "test@@mail.net",
+                "test@mai@l.net",
+                "test@mailnet",
+                "testmail.net",
+                "testmail.net",
+                "testmail.net",
+                "testmail.net",
+                "testmail.net",
+                "testmail.net"
+        );
+    }
+
+}
